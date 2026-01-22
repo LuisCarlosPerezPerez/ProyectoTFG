431512615d651456ac9ff3ef8320403428e526ae
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-01-22 12:53:55 +0100

    Creacion de la tabla Ingrediente

--- BBDD/Pasteleria.sql
+++ BBDD/Pasteleria.sql
@@ -8,3 +8,10 @@ Create Table CLIENTE (
     CONTRASEÑA VARCHAR(20) NOT NULL COMMENT 'Contraseña del usuario del Cliente',
     CONSTRAINT PK_ID_CLIENTE PRIMARY KEY (ID_CLIENTE)
 );
+
+CREATE TABLE INGREDIENTE(
+    ID_INGREDIENTE INT NOT NULL AUTO_INCREMENT COMMENT 'Identificador del Ingrediente',
+    NOMBRE VARCHAR(20) NOT NULL COMMENT 'Nombre del Ingrediente',
+    STOCK INT NOT NULL COMMENT 'Stock del Ingrediente',
+    PROVEEDOR VARCHAR(20) NOT NULL COMMENT 'Proveedor del Cliente'
+);
