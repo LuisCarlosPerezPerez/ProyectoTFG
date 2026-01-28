c74f4104d19879605b5f09a4e522b447b7a5320c
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-01-28 09:55:49 +0100

    Solucion de fallos

--- Entidades/Pasteleria/src/main/java/com/example/demo/repositorio/RepositorioEmpleado.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.EmpleadoEntity;
-
-public interface RepositorioEmpleado extends JpaRepository<EmpleadoEntity,Integer> {
-
-}
