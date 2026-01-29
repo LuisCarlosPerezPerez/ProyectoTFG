514fa91e17d38db2ac2df06a754ad81f2d54ffa1
LuisCarlos <luiscarlospe05@gmail.com>
2026-01-29 13:43:15 +0100

    "Subir las Implementaciones e Interfaz del ProductoIngrediente"

--- Entidades/Pasteleria/src/main/java/com/example/demo/repository/ProductosIngredientesRepository.java
+++ Entidades/Pasteleria/src/main/java/com/example/demo/repository/ProductosIngredientesRepository.java
@@ -5,10 +5,16 @@ import java.util.List;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.entity.IngredienteEntity;
+import com.example.demo.entity.ProductosEntity;
 import com.example.demo.entity.ProductosIngredientesEntity;
 
 public interface ProductosIngredientesRepository extends JpaRepository <ProductosIngredientesEntity, Integer> {
 
+	@Query("SELECT r FROM INGREDIENTE r WHERE r.ID_INGREDIENTE =: id")
+	IngredienteEntity ingrediente(int id);
 	
+	@Query("SELECT r FROM PRODUCTOS r WHERE r.ID_PRODUCTOS =: id")
+	ProductosEntity producto(int id);
 	
 }
---
+++ Entidades/Pasteleria/src/main/java/com/example/demo/services/interfaz/ProductoIngredienteInterfaz.java
@@ -0,0 +1,14 @@
+package com.example.demo.services.interfaz;
+
+import java.util.List;
+
+import com.example.demo.dto.ProductosIngredientesDTO;
+import com.example.demo.entity.ProductosEntity;
+import com.example.demo.entity.ProductosIngredientesEntity;
+
+public interface ProductoIngredienteInterfaz {
+
+	ProductosIngredientesDTO crearRelacion();
+	void GuardarRelacion(ProductosIngredientesDTO relacion);
+	List<ProductosIngredientesEntity> listarIngredientesProductos();
+}
