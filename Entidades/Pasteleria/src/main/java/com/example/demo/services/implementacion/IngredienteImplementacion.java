778f0ef586b3b6e73a43fdf2957ae6a8ceaf2e52
LuisCarlos <luiscarlospe05@gmail.com>
2026-02-05 11:45:38 +0100

    "imports para el funcionamiento del @Service"

--- Entidades/Pasteleria/src/main/java/com/example/demo/services/implementacion/IngredienteImplementacion.java
+++ Entidades/Pasteleria/src/main/java/com/example/demo/services/implementacion/IngredienteImplementacion.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.IngredientesDTO.*;
 import com.example.demo.dto.productoingrediente.ProductosIngredientesDTO;
--- Entidades/Pasteleria/src/main/java/com/example/demo/services/implementacion/RegistroImplementacion.java
+++ Entidades/Pasteleria/src/main/java/com/example/demo/services/implementacion/RegistroImplementacion.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.registro.*;
 import com.example.demo.entity.RegistroEntity;
