77183e70825e42758b37b6ba06d45c63cbf18681
LuisCarlos <luiscarlospe05@gmail.com>
2026-02-12 12:03:38 +0100

    fix: jUnit

--- Entidades/Pasteleria/src/test/java/com/example/demo/dto/IngredientesDTO/IngredienteCrearDtotest.java
+++ Entidades/Pasteleria/src/test/java/com/example/demo/dto/IngredientesDTO/IngredienteCrearDtotest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class IngredienteCrearDtotest {
 
 
